05e6744f422aa9bd8881a6a31274740282b2e4dc
AnniLevi <al.devpy@gmail.com>
2023-11-06 16:26:11 +0100

    implement a special attack functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -17,17 +18,32 @@ Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
+
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+
       // the monster will attack the player right after the player attacks the monster
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+
+      // deals more damage but is only available every 3 rounds
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+
+      this.attackPlayer();
     },
   },
 }).mount("#game");
